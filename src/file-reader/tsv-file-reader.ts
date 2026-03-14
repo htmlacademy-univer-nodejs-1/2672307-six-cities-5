ccb421053f06d0b6dd35228f9b8978a4095a5331
Ivan Meynart <ivan.meynart@gmail.com>
2026-03-14 14:11:37 +0300

    сделал задание

--- src/file-reader/tsv-file-reader.ts
+++ src/file-reader/tsv-file-reader.ts
@@ -1,22 +1,43 @@
 import { EventEmitter } from 'node:events';
-import { readFileSync } from 'node:fs';
+import { createReadStream } from 'node:fs';
 
 export class TSVFileReader extends EventEmitter {
   constructor(public filename: string) {
     super();
   }
 
-  public read(): void {
-    const content = readFileSync(this.filename, { encoding: 'utf-8' });
+  public async read(): Promise<void> {
+    const readStream = createReadStream(this.filename, {
+      highWaterMark: 16384, // Читаем кусками по 16KB
+      encoding: 'utf-8',
+    });
 
-    content
-      .split('\n')
-      .map((line) => line.trim())
-      .filter((line) => line.length > 0)
-      .forEach((line) => {
-        this.emit('line', line);
-      });
+    let remainingData = '';
+    let importedRowCount = 0;
 
-    this.emit('end');
+    for await (const chunk of readStream) {
+      remainingData += chunk.toString();
+      let nextLinePosition = remainingData.indexOf('\n');
+
+      while (nextLinePosition >= 0) {
+        const completeRow = remainingData.slice(0, nextLinePosition);
+        remainingData = remainingData.slice(nextLinePosition + 1);
+
+        if (completeRow.trim().length > 0) {
+          importedRowCount++;
+          this.emit('line', completeRow);
+        }
+
+        nextLinePosition = remainingData.indexOf('\n');
+      }
+    }
+
+    // Отдаем последний кусочек, если файл не закончился переносом строки
+    if (remainingData.trim().length > 0) {
+      importedRowCount++;
+      this.emit('line', remainingData);
+    }
+
+    this.emit('end', importedRowCount);
   }
 }
